ebc043876939f5e64681c7f81c1fb34bc3946bfa
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-04-02 16:55:49 -0400

    Iterator.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 struct Item {
     value: f64,
     weight: f64,
@@ -18,30 +17,50 @@ impl Item {
     }
 }
 
-fn main() {
-    let max_weight: f64 = 50.0;
-    let mut current_weight: f64 = 0.0;
-    let mut current_value: f64 = 0.0;
+struct KnapSack {
+    items: Vec<Item>,
+    max_weight: f64,
+    current_weight: f64,
+    max_value: f64,
+}
 
-    let mut items: Vec<Item> = vec![Item::new(60.0, 10.0), Item::new(100.0, 20.0), Item::new(120.0, 30.0)];
-
-    // Sort the list so that we add the items with the highest ratio first.
-    items.sort_by(|a, b| b.ratio().partial_cmp(&a.ratio()).unwrap());
-
-    // Choose items
-    for x in 0..items.len() {
-        if current_weight + items[x].weight <= max_weight {
-            current_weight += items[x].weight;
-            current_value += items[x].value;
-        } else {
-            // If this item is too large, we can pick a fractional part of it.
-            let remaining_weight = max_weight - current_weight;
-            current_value += items[x].value * (remaining_weight / items[x].weight);
-
-            // Bag is now full
-            current_weight = max_weight;
+impl KnapSack {
+    pub fn new(items: Vec<Item>, max_weight: f64) -> Self {
+        KnapSack {
+            items,
+            max_weight,
+            current_weight: 0.0,
+            max_value: 0.0,
         }
     }
 
-    println!("{:?}", current_value);
+    pub fn max_value(&mut self) -> f64 {
+        // Sort the list so that we add the items with the highest ratio first.
+        self.items.sort_by(|a, b| b.ratio().partial_cmp(&a.ratio()).unwrap());
+
+        // Choose items
+        let iter = self.items.iter();
+
+        for item in iter {
+            if self.current_weight + item.weight <= self.max_weight {
+                self.current_weight += item.weight;
+                self.max_value += item.value;
+            } else {
+                // If this item is too large, we can pick a fractional part of it.
+                let remaining_weight = self.max_weight - self.current_weight;
+                self.max_value += item.value * (remaining_weight / item.weight);
+
+                // Bag is now full
+                self.current_weight = self.max_weight;
+            }
+        }
+
+        self.max_value
+    }
+}
+
+fn main() {
+    let items: Vec<Item> = vec![Item::new(60.0, 10.0), Item::new(100.0, 20.0), Item::new(120.0, 30.0)];
+    let mut knapsack: KnapSack = KnapSack::new(items, 50.0);
+    println!("{}", knapsack.max_value());
 }
